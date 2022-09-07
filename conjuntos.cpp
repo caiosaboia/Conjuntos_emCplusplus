9583d4de21b5c3b91e14ea0a5fee4ce0999f253e
Caio R. Saboia <103676601+caiosaboia@users.noreply.github.com>
2022-09-07 08:59:42 -0300

    Update conjuntos.cpp

--- conjuntos.cpp
+++ conjuntos.cpp
@@ -75,24 +75,51 @@ bool Conjunto::push(int valor) {
 }
 
 void Conjunto::uniao(Conjunto& C1, Conjunto& C2) {
-	Conjunto C3 = C1;
-	for (int i = 0; i <= C2.tamanho(); i++) {
-			C3.push(i);
+	apaga_tudo(); int i, j;
+	int n1 = C1.tamanho();
+	int n2 = C2.tamanho();
+	if (n1 == 0) {
+		for (j = 0; j < n2; j++)
+			V.push_back(C2.V[i]);
+		return;
 	}
-	C3.imprime();
-	C3.apaga_tudo();
+	if (n2 == 0) {
+		for (i = 0; i < n1; i++) V.push_back(C1.V[i]);
+		return;
+	}
+	i = j = 0; int ultimo = min(C1.V[0], C2.V[0]);
+	if (ultimo == C1.V[0]) i++; else j++;
+	V.push_back(ultimo);
+
+	while ((i < n1) || (j < n2)) {
+		if ((j > n2) || ((i < n1) && (C1.V[i] <= C2.V[i]))) {
+			if (C1.V[i] > ultimo) {
+				V.push_back(C1.V[i]); ultimo = C1.V[i];
+			}
+			i++;
+		}
+		else {
+			if (C2.V[j] > ultimo) {
+				V.push_back(C2.V[j]); ultimo = C2.V[j];
+			}
+			j++;
+		}
+	}
 }
 
 void Conjunto::intersecao(Conjunto& C1, Conjunto& C2) {
-	Conjunto C3;
-	for (int i = 0; i <= C2.tamanho(); i++) {
-		if (C1.pertece(C2.V[i]) == true) {
-			C3.push(i);
+	if (C1.tamanho() > C2.tamanho()) {
+		intersecao(C2, C1);
+		return;
+	}
+	apaga_tudo();
+	for (int i = 0; i < C1.tamanho(); i++) {
+		if (C2.pertece(C1.V[i])) {
+			push(C1.V[i]);
 		}
 	}
-	C3.imprime();
-	C3.apaga_tudo();
 }
+
 
 int main() {
 	Conjunto C;
